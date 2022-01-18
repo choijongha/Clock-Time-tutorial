9cb3fd759290f6c9284fe2d462a13a4fc8557aea
choijongha <80575094+choijongha@users.noreply.github.com>
2022-01-18 20:13:15 +0900

    아날로그 시계 추가

--- Clock Time tutorial/Assets/Scenes/ClockAnimator.cs
+++ Clock Time tutorial/Assets/Scenes/ClockAnimator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ClockAnimator : MonoBehaviour
 {
+    public bool analog;
     // 시계는 360도 이고 시간은 그중 12, 분과 초는 60 으로 나눔
     private const float
         hoursToDegrees = 360f / 12f,
@@ -22,11 +23,22 @@ public class ClockAnimator : MonoBehaviour
     // 현재 시간 불러와 회전.
     void Update()
     {
-        // 현재 시간 불러오기.
-        DateTime time = DateTime.Now;
-        // Z축을 내려다보고 있고 Unity는 왼손 좌표계를 사용하므로 Z축을 중심으로 회전이 음수여야 함.
-        hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
-        minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
-        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        if (analog)
+        {
+            TimeSpan timespan  = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees);
+        }
+        else
+        {
+            // 현재 시간 불러오기.
+            DateTime time = DateTime.Now;
+            // Z축을 내려다보고 있고 Unity는 왼손 좌표계를 사용하므로 Z축을 중심으로 회전이 음수여야 함.
+            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        }
+        
     }
 }
